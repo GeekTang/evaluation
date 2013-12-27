973bf02a21407f3e6e335122a4aa01dcf41e43b1
Tang Weiqiang <weiqiang.tang@ericsson.com>
2013-12-27 18:14:07 +0800

    add example for xml support

---
+++ tryrest/README.md
@@ -0,0 +1,7 @@
+**Request**
+
+<xmp>
+<Service Version="v1" xmlns="http://www.github.com/GeekTang/evaluation">
+  <ServiceName xmlns="http://www.github.com/GeekTang/evaluation">Kill the first bug</ServiceName>
+</Service>
+</xmp>
--- tryrest/src/main/java/free/java/tryrest/KillBug.java
+++ tryrest/src/main/java/free/java/tryrest/KillBug.java
@@ -1,12 +1,29 @@
 package free.java.tryrest;
 
+import java.io.IOException;
+
+import org.restlet.ext.jaxb.JaxbRepresentation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
+
+import com.github.geektang.evaluation.Service;
 
 public class KillBug extends ServerResource {
 
     @Get
     public String toString() {
         return "Raid Kills Bugs Dead!";
+    }
+    
+    @Post
+    public String service(JaxbRepresentation<com.github.geektang.evaluation.Service> service) {
+        service.setContextPath("com.github.geektang.evaluation");
+        try {
+            Service request = service.getObject();
+            return request.getServiceName();
+        } catch (IOException e) {
+            return "Raid Kills Bugs Dead!";
+        }
     }
 }
